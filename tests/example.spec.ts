66af255f4628f69e718808128213ce73ebf30c34
siriwatknp <siriwatkunaporn@gmail.com>
2021-12-11 21:57:29 +0700

    add playwright

---
+++ tests/example.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from "@playwright/test";
+
+test("basic test", async ({ page, baseURL }) => {
+  await page.goto(baseURL || "https://playwright.dev/");
+  const title = page.locator(".navbar__inner .navbar__title");
+  await expect(title).toHaveText("Playwright");
+});
