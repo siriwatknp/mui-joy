a762d987f94e581f7db2e8736c3b27637b9df6f3
siriwatknp <siriwatkunaporn@gmail.com>
2021-12-12 09:45:13 +0700

    update deploy function

--- netlify/functions/deploy-succeeded.js
+++ netlify/functions/deploy-succeeded.js
@@ -3,6 +3,10 @@ const fetch = require("node-fetch");
 exports.handler = async (event, context) => {
   console.log("event", event);
   console.log("context", context);
+  console.log(
+    "process.env.CIRCLE_CI_API_TOKEN",
+    process.env.CIRCLE_CI_API_TOKEN
+  );
   try {
     await fetch(
       "https://circleci.com/api/v2/project/github/siriwatknp/mui-joy/pipeline",
@@ -10,9 +14,10 @@ exports.handler = async (event, context) => {
         method: "POST",
         headers: {
           "Content-type": "application/json",
+          Authorization: `Basic ${process.env.CIRCLE_CI_API_TOKEN}`,
         },
         body: JSON.stringify({
-          branch: "ci-setup",
+          branch: event.payload.branch,
           parameters: {
             workflow: "e2e-website",
             "playwright-base-url": "https://mui.com",
